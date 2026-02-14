d670464538bda7c0a35eb8b47be9e21a33379f4d
zhoujie1113 <zj19941113@gmail.com>
2026-02-14 13:47:31 +0800

    fea:更新产品信息

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -1,5 +1,5 @@
 export const PROJECT_FILE = 'AGENT.md'
-export const PRODUCT_NAME = 'Sema'
+export const PRODUCT_NAME = 'SemaCode'
 export const GROUP = 'AIRC'
-export const PRODUCT_URL = ''
+export const PRODUCT_URL = 'https://github.com/midea-ai/sema-code-core'
 
